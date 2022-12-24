5fd20cd1f018f844270dd877218b2d60e9d440af
quiencomotu03 <83849758+quiencomotu03@users.noreply.github.com>
2022-12-24 11:20:21 +0900

    FABRIK =forward and backward Reaching inverse Kinetics

--- Source/BlasterGame/Character/BlasterAnimInstance.h
+++ Source/BlasterGame/Character/BlasterAnimInstance.h
@@ -35,6 +35,8 @@ private:
 	UPROPERTY(BlueprintReadOnly, Category = Character, meta = (AllowPrivateAccess = "true"))
 	bool bWeaponEquipped;
 
+	class AWeapon* EquippedWeapon;
+
 	UPROPERTY(BlueprintReadOnly, Category = Character, meta = (AllowPrivateAccess = "true"))
 	bool bIsCrouched;
 
@@ -56,5 +58,8 @@ private:
 
 	UPROPERTY(BlueprintReadOnly, Category = Character, meta = (AllowPrivateAccess = "true"))
 	float AO_Pitch;
+
+	UPROPERTY(BlueprintReadOnly, Category = Character, meta = (AllowPrivateAccess = "true"))
+	FTransform LeftHandTransform;
 };
 
